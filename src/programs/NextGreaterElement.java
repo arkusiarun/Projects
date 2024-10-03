eaf0c56f164306be81a6b91b3db18d1c94d14bab
Arun Singh <arun5.singh@airtel.com>
2024-10-03 19:11:53 +0530

    New

---
+++ src/programs/NextGreaterElement.java
@@ -0,0 +1,34 @@
+package programs;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class NextGreaterElement {
+
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums2[i] > stack.peek()) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                map.put(nums2[i], -1);
+            } else {
+                map.put(nums2[i], stack.peek());
+            }
+            stack.push(nums2[i]);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = map.get(nums1[i]);
+        }
+        return nums1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{4, 1, 2};
+        int[] nums2 = new int[]{1, 3, 4, 2};
+        nextGreaterElement(nums1, nums2);
+    }
+}
