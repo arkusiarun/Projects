07215d9beef9e053a81dc6d5dcf2d732afe0ef53
Arun Singh <arun5.singh@airtel.com>
2024-10-04 13:38:36 +0530

    Program

---
+++ src/programs/ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+package programs;
+
+public class ContainerWithMostWater {
+
+    public static int maxArea(int[] A) {
+        int l = 0;
+        int r = A.length - 1;
+        int area = 0;
+        while (l < r) {
+            area = Math.max(area,
+                    Math.min(A[l], A[r]) * (r - l));
+
+            if (A[l] < A[r])
+                l += 1;
+
+            else
+                r -= 1;
+        }
+        return area;
+    }
+
+    public static void main(String[] args) {
+        int[] A = new int[]{1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(A));
+    }
+}
