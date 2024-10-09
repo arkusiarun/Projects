ff06f5bb705253b0abb3cd03353c479a45534dc0
Arun Singh <arun5.singh@airtel.com>
2024-10-09 11:21:46 +0530

    Streams

---
+++ src/programs/Java8Programs.java
@@ -0,0 +1,48 @@
+package programs;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Java8Programs {
+
+    public static void main(String[] args) {
+        getDistinctElements();
+        getAllEven();
+        getCountOfEveryElement();
+    }
+
+    static void getDistinctElements() {
+        int[] arr = new int[]{1,2,2,1,3,4};
+        List<Integer> list = Arrays.stream(arr).distinct().mapToObj(Integer::new).collect(Collectors.toList());
+        for(Integer n: list) {
+            System.out.println(n);
+        }
+    }
+
+    static void getAllEven() {
+        int[] arr = new int[]{1,2,3,4,5,6};
+        Map<Boolean, List<Integer>> result = Arrays.stream(arr).mapToObj(Integer::new).collect(Collectors.partitioningBy(i -> i % 2 == 0));
+        for(boolean key: result.keySet()) {
+            System.out.println("Key : " + key);
+            System.out.print("Value : ");
+            for(Integer value: result.get(key)) {
+                System.out.print(value + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    static void getCountOfEveryElement() {
+        int[] arr = new int[]{1,2,1,4,5,6};
+        Map<Integer, Long> result = Arrays.stream(arr).mapToObj(Integer::new).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        for(Integer key: result.keySet()) {
+            System.out.println("Key : " + key);
+            System.out.println("Value : " + result.get(key));
+        }
+    }
+
+}
