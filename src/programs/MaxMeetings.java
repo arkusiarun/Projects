083c94ae6e48d7f60c8bb7df01481b253225308e
Arun Singh <arun5.singh@airtel.com>
2024-10-25 08:26:23 +0530

    Max Number of meetings

--- src/programs/MaxMeetings.java
+++ src/programs/MaxMeetings.java
@@ -8,7 +8,6 @@ import java.util.Comparator;
 
 public class MaxMeetings {
 
-    // A class to store start and end times of meetings
     static class Meeting {
         int start, end;
 
@@ -18,28 +17,23 @@ public class MaxMeetings {
         }
     }
 
-    // Function to find the maximum number of meetings that can happen in the boardroom
     public static int maxMeetings(Meeting[] meetings) {
-        // Sort meetings by their end time
         Arrays.sort(meetings, Comparator.comparingInt(meeting -> meeting.end));
 
         int count = 0;
         int lastEndTime = -1;
 
-        // Iterate through the meetings
         for (Meeting meeting : meetings) {
-            // If the meeting starts after the last selected meeting ends
             if (meeting.start > lastEndTime) {
-                count++;  // select this meeting
-                lastEndTime = meeting.end;  // update the end time of the last selected meeting
+                count++;
+                lastEndTime = meeting.end;
             }
         }
 
-        return count;  // return the maximum number of meetings
+        return count;
     }
 
     public static void main(String[] args) {
-        // Example: Meetings with start and end times
         Meeting[] meetings = {
                 new Meeting(1, 2),
                 new Meeting(3, 4),
@@ -48,8 +42,6 @@ public class MaxMeetings {
                 new Meeting(8, 9),
                 new Meeting(5, 9)
         };
-
-        // Find and print the maximum number of meetings
         System.out.println("Maximum number of meetings: " + maxMeetings(meetings));
     }
 }
