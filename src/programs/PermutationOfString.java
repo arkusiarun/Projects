cb4fb3574f12bf41163a35762020eaacff86f8d2
Arun Kumar Singh <arunkrsingh111@gmail.com>
2024-11-19 19:26:03 +0530

    Permutation using stream

--- src/programs/PermutationOfString.java
+++ src/programs/PermutationOfString.java
@@ -1,14 +1,33 @@
 package programs;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class PermutationOfString {
 
+    public static List<String> getPermutations(String str) {
+        if (str == null || str.isEmpty()) {
+            return List.of("");
+        }
+
+        return IntStream.range(0, str.length())
+                .boxed()
+                .flatMap(i -> {
+                    char currentChar = str.charAt(i);
+                    String remaining = str.substring(0, i) + str.substring(i + 1);
+                    return getPermutations(remaining).stream().map(permutation -> currentChar + permutation);
+                })
+                .collect(Collectors.toList());
+    }
+
     static void permute(String str, int start, int end) {
-        if(start == end) {
+        if (start == end) {
             System.out.println(str);
         }
-        for(int i=start;i<=end;i++) {
+        for (int i = start; i <= end; i++) {
             str = swap(str, start, i);
-            permute(str, start+1, end);
+            permute(str, start + 1, end);
             str = swap(str, start, i);
         }
     }
@@ -23,6 +42,10 @@ public class PermutationOfString {
 
     public static void main(String[] args) {
         String str = "abc";
-        permute(str, 0, str.length()-1);
+        //permute(str, 0, str.length() - 1);
+        List<String> result = getPermutations(str);
+        for(String s: result) {
+            System.out.println(s);
+        }
     }
 }
