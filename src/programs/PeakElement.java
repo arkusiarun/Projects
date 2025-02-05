32b56ccebbcae55478ff7156a13e876021494773
Arun Kumar Singh <arunkrsingh111@gmail.com>
2025-02-05 12:50:53 +0530

    New Program

--- src/programs/PeakElement.java
+++ src/programs/PeakElement.java
@@ -4,21 +4,34 @@ public class PeakElement {
     public int findPeakElement(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public int findPeakElementDuplicate(int[] nums) {
+        int left = 0, right = nums.length - 1;
 
         while (left < right) {
             int mid = left + (right - left) / 2;
 
             if (nums[mid] > nums[mid + 1]) {
-                // You are in the decreasing part of the array.
-                // This means the peak is on the left side (including mid).
                 right = mid;
-            } else {
-                // You are in the increasing part of the array.
+            } else if (nums[mid] < nums[mid + 1]) {
                 left = mid + 1;
+            } else {
+                left++;
+                right--;
             }
         }
-
-        // When left == right, we have found the peak element.
         return left;
     }
+
 }
